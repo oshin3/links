9426a9194d58f53822ffb4ee01b1b1aac828e432
shin <shin3.prg@gmail.com>
2021-09-19 11:51:25 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,7 @@ class ApplicationController < ActionController::Base
 
   def counts(user)
     @count_links = user.links.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
